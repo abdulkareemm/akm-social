a2f9b1e19aeeb7420b9538450a1e966c44bd30f1
abdulkareemm <eng.kreem.96@gmail.com>
2023-01-15 00:35:22 -0800

    fix

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import { ForwardArrow, Logo } from "./img";
+import { MainContainer } from "./components";
+
+import {
+  Add,
+  Chat,
+  FavoriteRounded,
+  Notifications,
+  Person,
+  QuestionMark,
+} from "@mui/icons-material";
+import { useEffect } from "react";
+function App() {
+  useEffect(() => {
+    const allIcon = document.querySelectorAll(".icon-container");
+    function setMenuActive() {
+      allIcon.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    allIcon.forEach((n) => n.addEventListener("click", setMenuActive));
+  }, []);
+  return (
+    <div className="App">
+      <div className="menuContainer">
+        <img src={Logo} alt="logo" className="logo" />
+        <div className="subMenu">
+          <div>
+            <MainContainer Icon={Person} />
+            <MainContainer Icon={Notifications} />
+            <MainContainer Icon={Chat} />
+          </div>
+          <div>
+            <MainContainer Icon={FavoriteRounded} />
+          </div>
+          <div>
+            <MainContainer Icon={QuestionMark} />
+            <MainContainer Icon={Add} />
+          </div>
+        </div>
+      </div>
+      <main>
+      <div className="searchBox">
+        <input type="text" placeholder="Search..."/>
+        <div className="search">
+          <img src={ForwardArrow} alt=""/>
+          
+        </div>
+      </div>
+      <div className="main-container">
+        
+      </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mainContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const mainContainer = ({Icon}) => {
+  return (
+    <div className='icon-container'>
+    <Icon/>
+    </div>
+  )
+}
+
+export default mainContainer
